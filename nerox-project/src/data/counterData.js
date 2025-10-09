d962e64bdc079bc8e23ef4ea00615939b5382196
MartaNS09 <martacrystal9@gmail.com>
2025-10-09 15:33:33 +0300

    Перевел счетчики на русский язык

--- nerox-project/src/data/counterData.js
+++ nerox-project/src/data/counterData.js
@@ -1,33 +1,33 @@
 const counterData = [
   {
     id: 1,
-    icon: 'flaticon-customers',
-    iconImg: 'assets/img/icon/fact/fact-icon-1.png',
-    number: 329,
-    title: 'Satisfied customers'
+    icon: "flaticon-customers",
+    iconImg: "assets/img/icon/fact/fact-icon-1.png",
+    number: 219,
+    title: "Постоянных клиентов",
   },
   {
     id: 2,
-    icon: 'flaticon-tasks',
-    iconImg: 'assets/img/icon/fact/fact-icon-2.png',
+    icon: "flaticon-tasks",
+    iconImg: "assets/img/icon/fact/fact-icon-2.png",
     number: 89,
-    title: 'Completed projects'
+    title: "Реализованных проектов",
   },
   {
     id: 3,
-    icon: 'flaticon-success',
-    iconImg: 'assets/img/icon/fact/fact-icon-3.png',
+    icon: "flaticon-success",
+    iconImg: "assets/img/icon/fact/fact-icon-3.png",
     number: 149,
-    title: 'World awards'
+    title: "SEO-продвижений",
   },
   {
     id: 4,
-    icon: 'flaticon-coffee',
-    iconImg: 'assets/img/icon/fact/fact-icon-4.png',
+    icon: "flaticon-coffee",
+    iconImg: "assets/img/icon/fact/fact-icon-4.png",
     number: 208,
-    title: 'Cups of coffee',
+    title: "'Технических решений",
     border: false,
   },
-]
+];
 
 export default counterData;
