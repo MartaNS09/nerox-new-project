7313c1a58308abde4dbe57cc3e59895a762c2d6a
MartaNS09 <martacrystal9@gmail.com>
2025-10-09 14:55:49 +0300

    Заменил награды на преимущества веб-студии

--- nerox-project/src/components/About/NeroxAwards.jsx
+++ nerox-project/src/components/About/NeroxAwards.jsx
@@ -1,7 +1,53 @@
-import awardData from '../../data/awardsData';
+// import awardData from "../../data/awardsData";
+
+// const NeroxAwards = () => {
+//   return (
+//     <>
+//       <div className="tp-award-area pt-135 pb-120">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-xl-12">
+//               <div className="section-wrapper text-center">
+//                 <h3 className="tpabout-xd-title mb-15">
+//                   <b>Наши преимущества</b>
+//                 </h3>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="row mt-35">
+//             <div className="col-xl-12">
+//               {awardData.map((item) => (
+//                 <div key={item.id} className="tp-award-item mb-30">
+//                   <div
+//                     style={{ padding: "20px 0" }}
+//                     className="award-inner d-md-flex align-items-center"
+//                   >
+//                     <div className="award-image">
+//                       <img src={item.img} alt="award-img" />
+//                     </div>
+//                     <div className="award-content">
+//                       <h4 className="award-title">{item.title}</h4>
+//                       <span className="award-meta">
+//                         <span className="award-year">2022..</span> MNX Business
+//                         Card
+//                       </span>
+//                     </div>
+//                   </div>
+//                 </div>
+//               ))}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default NeroxAwards;
+
+import advantageData from "../../data/awardsData";
 
 const NeroxAwards = () => {
-
   return (
     <>
       <div className="tp-award-area pt-135 pb-120">
@@ -9,30 +55,29 @@ const NeroxAwards = () => {
           <div className="row">
             <div className="col-xl-12">
               <div className="section-wrapper text-center">
-                <h3 className="tpabout-xd-title mb-15"><b>Neorox Awards</b></h3>
+                <h3 className="tpabout-xd-title mb-15">
+                  <b>Наши преимущества</b>
+                </h3>
               </div>
             </div>
           </div>
           <div className="row mt-35">
             <div className="col-xl-12">
-              {
-                awardData.map(item => (
-                  <div key={item.id} className="tp-award-item mb-30">
-                    <div style={{ padding: "20px 0" }} className="award-inner d-md-flex align-items-center">
-                      <div className="award-image">
-                        <img src={item.img} alt="award-img" />
-                      </div>
-                      <div className="award-content">
-                        <h4 className="award-title">{item.title}
-                        </h4>
-                        <span className="award-meta">
-                          <span className="award-year">2022..</span>  MNX Business Card</span>
-                      </div>
+              {advantageData.map((item) => (
+                <div key={item.id} className="tp-award-item mb-30">
+                  <div
+                    style={{ padding: "20px 0" }}
+                    className="award-inner d-md-flex align-items-center"
+                  >
+                    <div className="award-image">
+                      <img src={item.img} alt="advantage-img" />
+                    </div>
+                    <div className="award-content">
+                      <h4 className="award-title">{item.title}</h4>
                     </div>
                   </div>
-                ))
-              }
-
+                </div>
+              ))}
             </div>
           </div>
         </div>
--- nerox-project/src/data/awardsData.js
+++ nerox-project/src/data/awardsData.js
@@ -1,24 +1,49 @@
-const awardData = [
+// const awardData = [
+//   {
+//     id: 1,
+//     img: 'assets/img/award/award-img-1.png',
+//     title: '1st Winner Portfolio Review US 2019'
+//   },
+//   {
+//     id: 2,
+//     img: 'assets/img/award/award-img-2.png',
+//     title: 'Websites Of The Current Year 2022'
+//   },
+//   {
+//     id: 3,
+//     img: 'assets/img/award/award-img-3.png',
+//     title: 'Best 20 Websites Favourite 2020'
+//   },
+//   {
+//     id: 4,
+//     img: 'assets/img/award/award-img-4.png',
+//     title: 'Design Inspiration For Team  #56'
+//   },
+// ]
+
+// export default awardData;
+
+const advantageData = [
   {
     id: 1,
-    img: 'assets/img/award/award-img-1.png',
-    title: '1st Winner Portfolio Review US 2019'
+    img: "assets/img/award/award-img-1.png",
+    title: "Сайты под ключ с гарантией результата",
   },
   {
     id: 2,
-    img: 'assets/img/award/award-img-2.png',
-    title: 'Websites Of The Current Year 2022'
+    img: "assets/img/award/award-img-2.png",
+    title: "SEO-продвижение в ТОП поисковых систем",
   },
   {
     id: 3,
-    img: 'assets/img/award/award-img-3.png',
-    title: 'Best 20 Websites Favourite 2020'
+    img: "assets/img/award/award-img-3.png",
+    title: "Мобильная разработка и адаптивный дизайн",
   },
   {
     id: 4,
-    img: 'assets/img/award/award-img-4.png',
-    title: 'Design Inspiration For Team  #56'
+    img: "assets/img/award/award-img-4.png",
+    title: "Техническая поддержка 24/7",
   },
-]
+];
 
-export default awardData;
+export default advantageData;
